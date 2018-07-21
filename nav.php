3892206d8a9e2519eb311e234815d0ca472864ea
Zaibapari <41237989+Zaibapari@users.noreply.github.com>
2018-07-21 20:45:32 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/add.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head>
+<body background="img/an.jpg">
+
+<header class="header">
+<nav class="navbar navbar-style">
+<div  class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	</button><h1>employee management</h1>
+</div>
+<div class="collapse navbar-collapse" id="micon">
+	<ul class="nav navbar-nav navbar-right">
+	<li><a href="add.php">Add Employee</a></li>
+		<li><a href="details.php">Employee Details</a></li>
+		<li><a href="delete.php">Delete Employee</a></li>
+		<li><a href="update.php">Update Employee</a></li>
+		<li><a href="search.php">Search Employee</a></li>
+		<li><a href="logout.php">Logout</a></li>
+	</ul>
+</div>
+</div>
+	</nav>
+	</header>
+	</body>
+	</html>
